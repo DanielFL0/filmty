f1c656e16f650be1b82ebd776dfdaa5a12095fe9
DanielFl0 <jose.saldanafrs@uanl.edu.mx>
2020-11-02 18:21:14 -0600

    Add views.py

--- films/views.py
+++ films/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import User, Movie, Review
 
 # Create your views here.
+def index(request):
+    latest_movies_list = Movie.objects.order_by('release_date')[:5]
+    context = {'latest_movies_list': latest_movies_list}
+    return render(request, 'films/index.html', context)
