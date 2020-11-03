3a3a4edbd4f0acc1e73c2c19c6eda6a045b86f35
DanielFl0 <jose.saldanafrs@uanl.edu.mx>
2020-11-02 18:21:04 -0600

    Add models.py

--- films/models.py
+++ films/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    firstname = models.CharField(max_length=25)
+    lastname = models.CharField(max_length=25)
+    username = models.CharField(max_length=20)
+    password = models.CharField(max_length=255)
+    email = models.CharField(max_length=50)
+    profile_picture = models.FileField(upload_to='profile_pictures')
+
+    def __str__(self):
+        return self.username
+
+class Movie(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=500)
+    release_date = models.DateTimeField('release date')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    thumbnail = models.FileField(upload_to='thumbnails')
+    video = models.FileField(upload_to='videos')
+
+    def __str__(self):
+        return self.name
+
+class Review(models.Model):
+    content = models.CharField(max_length=1200)
+    date_published = models.DateTimeField('date published')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.content
