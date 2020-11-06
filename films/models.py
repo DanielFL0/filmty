ff4e75e0fb35aefb624fd9c09e5ff3d18c1c53e2
DanielFl0 <jose.saldanafrs@uanl.edu.mx>
2020-11-06 12:53:29 -0600

    Modify the user model

--- films/models.py
+++ films/models.py
@@ -1,12 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-class User(models.Model):
-    firstname = models.CharField(max_length=25)
-    lastname = models.CharField(max_length=25)
-    username = models.CharField(max_length=20)
-    password = models.CharField(max_length=255)
-    email = models.CharField(max_length=50)
+class ForumUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     profile_picture = models.FileField(upload_to='profile_pictures')
 
     def __str__(self):
