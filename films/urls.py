41b8c38c5a946ff0a34ec136bb3019f1a33a47b8
DanielFl0 <jose.saldanafrs@uanl.edu.mx>
2020-11-06 12:53:54 -0600

    Modify urls.py

--- films/urls.py
+++ films/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'films'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('catalog/', views.catalog, name='catalog'),
+    path('movie/<int:movie_id>/', views.movie, name='movie'),
+    path('login/', views.user_login, name='login'),
+    path('register/', views.user_register, name='register'),
 ]
