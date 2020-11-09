da666b7deed8aa7a651acffacce9878dac2ba1eb
DanielFl0 <jose.saldanafrs@uanl.edu.mx>
2020-11-08 19:40:28 -0600

    Modify admin.py

--- films/admin.py
+++ films/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from .models import ForumUser, Movie, Review
+from .models import ForumUser, Movie, Category, Review
 
 # Register your models here.
+admin.site.register(ForumUser)
+admin.site.register(Movie)
+admin.site.register(Category)
+admin.site.register(Review)
